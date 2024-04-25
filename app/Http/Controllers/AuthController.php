cb89ed1284e39386a89e35fe5a57de4d9bc33a49
tainamiranda2 <tainaferreira0987@gmail.com>
2024-04-25 16:19:05 -0300

    continuacao da aula

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Testing\Concerns\MakesHttpRequests;
+use Illuminate\Http\Request;
+use Illuminate\Http\ResponseTrait;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+//PY5uq08J9O30MipBcyFlFYKB0yH60m86XWxwyigX0bc9c276
+   // use MakesHttpRequests;
+    public function login (Request $request){
+       if(Auth::attempt($request->only('email', 'password'))){
+        $token = $request->user()->createToken('academia')->plainTextToken;
+        return response('Authorized', 200)->header('Authorizationnnn', 'Bearer ' . $token);
+       }else{
+        return response('Not Authorized', 403);
+
+       }
+    }
+
+    public function logout (){
+        
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,18 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AcademiaController;
+use App\Http\Controllers\Api\V1\USerController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
+
+Route::get('/academia', [AcademiaController::class, 'index']);
+Route::get('/user', [USerController::class, 'index']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::post('/teste', [AuthController::class, 'login']);
